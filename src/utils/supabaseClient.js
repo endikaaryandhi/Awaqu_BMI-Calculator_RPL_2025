0a88faa73d9bbc64bf664ef5040fa2b82378b556
endikaaryandhi <endikaaryandhi15@gmail.com>
2025-05-27 19:48:26 +0700

    No .env

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rxxipydjomsycacdfoqp.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4eGlweWRqb21zeWNhY2Rmb3FwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4OTU2OTAsImV4cCI6MjA2MzQ3MTY5MH0.T630P6tBrkc63Ru2f78DNalTzCLxXLsitXs8i5qoMhE';
+import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+        
